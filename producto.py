9bfd0c052e9159f6417dc4b430019f39d564a8be
ppair <ppc85@alu.ua.es>
2026-03-22 04:24:48 +0100

    He convertido la clase producto en clase abstracta, y los atributos los converti privados con sus respectivos getters.

--- producto.py
+++ producto.py
@@ -1,45 +1,52 @@
-class Producto:
-    def __init__(self, nombre, precio, stock):
-        if precio < 0:
+from abc import ABC, abstractmethod
+
+class Producto(ABC):
+    """"CLASE ABSTRACTA | PRODUCTO BASE DEL MENU DEL FOOD TRUCK"""
+
+    def __init__(self, nombre: str, precio: float, stock: int):
+        if precio <= 0:
             raise ValueError("Precio no valido")
         if stock < 0:
             raise ValueError("Stock no valido")
 
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
+        self.__nombre = nombre
+        self.__precio = float(precio)
+        self.__stock = stock
 
-    def __str__(self):
-        return f'{self.nombre} — {self.precio:.2f}€  [stock: {self.stock}]'
-    def __repr__(self):
-        return f'(nombre={self.nombre}, precio={self.precio}, stock={self.stock})'
+    #------------------------------------
+    # GETTERS DE LOS ATRIBUTOS PRIVADOS
+    #------------------------------------
+    @property
+    def nombre(self) -> str:
+        return self.__nombre
+
+    @property
+    def precio_base(self) -> float:
+        return self.__precio
+
+    @property
+    def stock(self) -> int:
+        return self.__stock
 
     #--------------------------------------
     # GESTIÓN DE STOCK
     #--------------------------------------
-
-    def reponer_stock(self, cantidad):
+    def reponer_stock(self, cantidad: int):
         if cantidad <= 0:
             raise ValueError("No se puede reponer una cantidad negativa")
-        self.stock += cantidad
+        self.__stock += cantidad
 
-    def reducir_stock(self, cantidad):
-        if cantidad > self.stock:
+    def reducir_stock(self, cantidad: int):
+        if cantidad <= 0:
+            raise ValueError('La cantidad a descontar debe ser positiva')
+        if cantidad > self.__stock:
             raise ValueError("No se puede reducir a un stock negativo")
-        self.stock -= cantidad
+        self.__stock -= cantidad
 
-#-------------------------------
-# PRUEBA
-#-------------------------------
-
-hamburguesa=Producto('Hamburguesa',12.30, 120)
-agua=Producto('Agua',1.20, 200)
-
-print(hamburguesa)
-print(agua)
-
-hamburguesa.reponer_stock(100)
-agua.reducir_stock(100)
-
-print(hamburguesa)
-print(agua)
+    #--------------------------------------
+    # MÉTODOS ESPECIALES
+    #--------------------------------------
+    def __str__(self):
+        return f'{self.__nombre} — {self.__precio:.2f}€  [stock: {self.__stock}]'
+    def __repr__(self):
+        return f'(nombre={self.__nombre}, precio={self.__precio}, stock={self.__stock})'

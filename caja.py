9e254f8ec6486e182c757fa7a4e18ce2c8b6866c
MiguelKan <mapr22@alu.ua.es>
2026-03-29 23:23:54 +0200

    Creada la clase Caja

--- caja.py
+++ caja.py
@@ -3,3 +3,25 @@ class Caja:
 
     def __init__(self):
         self.__saldo = 0.0
+
+    # GETTER
+    @property
+    def saldo(self) -> float:
+        return self.__saldo
+
+    # MÉTODOS
+    def registrar_ingreso(self, cantidad: float):
+        if cantidad <= 0:
+            raise ValueError("La cantidad debe ser positiva")
+        self.__saldo += cantidad
+
+    def registrar_pedido(self, pedido):
+        """Añade el dinero de un pedido a la caja"""
+        self.registrar_ingreso(pedido.total)
+
+    def reiniciar_caja(self):
+        """Reinicia el saldo (ej: fin de día)"""
+        self.__saldo = 0.0
+
+    def __str__(self):
+        return f"Caja actual: {self.__saldo:.2f}€"

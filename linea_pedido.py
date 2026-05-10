cd4edb346a0045ebc88f45a83217902ba9689810
ppair <ppc85@alu.ua.es>
2026-05-10 22:33:19 +0200

    Añado clase LineaPedido para gestionar productos y cantidades

---
+++ linea_pedido.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from producto import Producto
+
+
+@dataclass
+class LineaPedido:
+    producto: Producto
+    cantidad: int
+
+    def __post_init__(self) -> None:
+        if self.cantidad <= 0:
+            raise ValueError('La cantidad debe ser positiva.')
+
+    @property
+    def subtotal(self) -> float:
+        return round(self.producto.precio_venta() * self.cantidad, 2)
+
